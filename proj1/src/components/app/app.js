06c11bbc57b1fed9d014c3e3d4bbe8c23c3bc1b5
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-21 22:56:14 +0300

    les92 props and states

--- proj1/src/components/app/app.js
+++ proj1/src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form'
 import './app.css'
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', id: 'fsdc'},
+        {label: 'That is so good', id: 'sdfe'},
+        {label: 'I need a break...', id: 'hbfg'}
+    ]
+
     return (
         <div className='app'>
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
--- proj1/src/components/post-list/post-list.js
+++ proj1/src/components/post-list/post-list.js
@@ -4,14 +4,23 @@ import PostListItem from '../post-list-item'
 
 import './post-list.css'
 
-const PostList = () => {
+const postList = ({posts}) => {
+
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item
+
+        return (
+            <li className='list-group-item' key={id}>
+                <PostListItem {...itemProps} />
+            </li>
+        )
+    })
+
     return (
         <ul className='app-list list-group'>
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
 
-export default PostList
+export default postList
