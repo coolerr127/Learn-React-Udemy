59535f74a1c3e232593a0a8ef3b623835fdeb828
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-25 00:46:15 +0300

    start proj3

---
+++ proj3/src/index.js
@@ -0,0 +1,13 @@
+const reducer = (state = 0, action) => {
+	switch (action.type) {
+	case 'INC':
+		return state + 1
+	default:
+		return state
+	}
+}
+
+let state = reducer(undefined, { type: 'INC' })
+console.log(state)
+state = reducer(state, { type: 'INC' })
+console.log(state)
