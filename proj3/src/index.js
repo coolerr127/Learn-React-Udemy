a8bcb584ea84aeafc22d66331ce12c2adc282337
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-26 19:48:39 +0300

    started learning redux

--- proj3/src/index.js
+++ proj3/src/index.js
@@ -1,13 +1,44 @@
+import { createStore } from 'redux'
+
 const reducer = (state = 0, action) => {
 	switch (action.type) {
 	case 'INC':
 		return state + 1
+	case 'DEC':
+		return state - 1
+	case 'RND':
+		return state + action.value
 	default:
 		return state
 	}
 }
 
-let state = reducer(undefined, { type: 'INC' })
-console.log(state)
-state = reducer(state, { type: 'INC' })
-console.log(state)
+const inc = () => ({ type: 'INC' })
+const dec = () => ({ type: 'DEC' })
+const rnd = (value) => ({type: 'RND', value})
+
+const store = createStore(reducer)
+
+document.getElementById('inc').addEventListener('click', () => {
+	store.dispatch(inc())
+})
+document.getElementById('dec').addEventListener('click', () => {
+	store.dispatch(dec())
+})
+document.getElementById('rnd').addEventListener('click', () => {
+	const value = Math.floor(Math.random() * 10 + 1)
+	store.dispatch(rnd(value))
+})
+
+const update = () => {
+	document.getElementById('counter').textContent = store.getState()
+}
+
+store.subscribe(update)
+
+// store.dispatch({
+// 	type: 'INC',
+// })
+// store.dispatch({
+// 	type: 'INC',
+// })
